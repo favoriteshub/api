e2a76cc788926f656e20152c509087ee50d603b8
Cristi Ciobanu <c-ciobanu@protonmail.com>
2018-08-30 22:33:12 +0200

    [S] added swagger documentation

--- server.js
+++ server.js
@@ -16,12 +16,22 @@ passport.use(passportStrategy());
 
 const app = require("express")();
 const bodyParser = require("body-parser");
+const swaggerUi = require("swagger-ui-express");
+const JsonRefs = require("json-refs");
 
 app.use(passport.initialize());
 app.use(require("cors")());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+JsonRefs.resolveRefsAt("./documentation/swagger.json").then(
+  (res) => {
+    app.use("/swagger", swaggerUi.serve, swaggerUi.setup(res.resolved));
+  },
+  (err) => {
+    console.log(err.stack);
+  }
+);
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/shows", passport.authenticate("jwt", {session: false}), require("./routes/shows"));
 app.use("/api/users", passport.authenticate("jwt", {session: false}), require("./routes/users"));
