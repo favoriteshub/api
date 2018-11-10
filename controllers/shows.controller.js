6bb92dc7c930d43d98250291eea4d0262f7bfe31
Cristi Ciobanu <c-ciobanu@protonmail.com>
2018-11-10 14:15:26 +0100

    [S] shows search response added isAssociated property

--- controllers/shows.controller.js
+++ controllers/shows.controller.js
@@ -3,60 +3,65 @@ const searchConfig = require("../configuration/search.json");
 const {to, resErr, resSucc} = require("../services/response");
 
 const newShow = async (req, res) => {
-  const [err, show] = await to(Show.create(req.body));
+	const [err, show] = await to(Show.create(req.body));
 
-  if (err) {
-    return resErr(res, err);
-  }
-  return resSucc(res, show);
+	if (err) {
+		return resErr(res, err);
+	}
+	return resSucc(res, show);
 };
 
 const searchByTitleCount = async (req, res) => {
-  const [err, count] = await to(Show.count({title: {$regex: req.query.title, $options: "i"}}));
+	const [err, count] = await to(Show.count({title: {$regex: req.query.title, $options: "i"}}));
 
-  if (err) {
-    return resErr(res, err);
-  }
-  return resSucc(res, {
-    count,
-    pages: Math.ceil(count / searchConfig.elementsPerPage)
-  });
+	if (err) {
+		return resErr(res, err);
+	}
+	return resSucc(res, {
+		count,
+		pages: Math.ceil(count / searchConfig.elementsPerPage)
+	});
 };
 
 const searchByTitlePaged = async (req, res) => {
-  let elementsPerPage = searchConfig.elementsPerPage;
+	let elementsPerPage = searchConfig.elementsPerPage;
 
-  const [err, shows] = await to(
-    Show.find({title: {$regex: req.query.title, $options: "i"}})
-      .sort({title: 1})
-      .skip(elementsPerPage * req.params.page)
-      .limit(elementsPerPage)
-  );
+	const [err, shows] = await to(
+		Show.find({title: {$regex: req.query.title, $options: "i"}})
+			.sort({title: 1})
+			.skip(elementsPerPage * req.params.page)
+			.limit(elementsPerPage)
+			.lean()
+	);
 
-  if (err) {
-    return resErr(res, err);
-  }
-  return resSucc(res, shows);
+	if (err) {
+		return resErr(res, err);
+	} else {
+		let associatedShows = req.user.library.shows.map((show) => show.toString());
+		let returnedShows = shows.map((show) => ({...show, isAssociated: associatedShows.includes(show._id.toString())}));
+
+		return resSucc(res, returnedShows);
+	}
 };
 
 const getOne = async (req, res) => {
-  const [err, show] = await to(Show.findById(req.params.id));
+	const [err, show] = await to(Show.findById(req.params.id));
 
-  if (err) {
-    return resErr(res, err);
-  } else if (!show) {
-    return resErr(res, "This show does not exist");
-  }
-  return resSucc(res, show);
+	if (err) {
+		return resErr(res, err);
+	} else if (!show) {
+		return resErr(res, "This show does not exist");
+	}
+	return resSucc(res, show);
 };
 
 const updateOne = async (req, res) => {
-  const [err, data] = await to(Show.updateOne({_id: req.params.id}, req.body));
+	const [err, data] = await to(Show.updateOne({_id: req.params.id}, req.body));
 
-  if (err) {
-    return resErr(res, err);
-  }
-  return resSucc(res, data);
+	if (err) {
+		return resErr(res, err);
+	}
+	return resSucc(res, data);
 };
 
 module.exports = {newShow, searchByTitleCount, searchByTitlePaged, getOne, updateOne};
