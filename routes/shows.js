5cf1642799c16bdb90ff43e904b906855b02e40c
Cristi Ciobanu <c-ciobanu@protonmail.com>
2018-12-30 00:49:48 +0100

    [S] removed old shows controller

--- routes/shows.js
+++ routes/shows.js
@@ -1,20 +1,9 @@
 const router = require("express").Router();
-const showsController = require("../controllers/shows");
-const thetvdbController = require("../controllers/the-tv-db");
+const controller = require("../controllers/the-tv-db");
 
-router.route("/").post(showsController.newShow);
+router.route("/search").get(controller.searchByName);
 
-// router.route("/search/count").get(showsController.searchByTitleCount);
-// router.route("/search").get(showsController.searchByTitlePaged);
-
-router.route("/search").get(thetvdbController.searchByName);
-
-router.route("/:id").get(thetvdbController.seriesInfo);
-router.route("/:id/seasons/:season").get(thetvdbController.seriesSeason);
-
-router
-	.route("/:id")
-	.get(showsController.getOne)
-	.put(showsController.updateOne);
+router.route("/:id").get(controller.seriesInfo);
+router.route("/:id/seasons/:season").get(controller.seriesSeason);
 
 module.exports = router;
