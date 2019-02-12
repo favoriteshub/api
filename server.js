4457d5b205479a22532aa51a34a920ee55632338
Cristi Ciobanu <c-ciobanu@protonmail.com>
2019-02-12 20:31:12 +0100

    remove documentation

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const express = require("express");
 const passport = require("./services/passport");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const JsonRefs = require("json-refs");
-const swaggerUi = require("swagger-ui-express");
 
 mongoose.connect(process.env.DB).then(
 	() => console.log("Successfully connected to the database"),
@@ -23,14 +21,6 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-JsonRefs.resolveRefsAt("./documentation/swagger.json").then(
-	(response) => {
-		app.use("/swagger", swaggerUi.serve, swaggerUi.setup(response.resolved));
-	},
-	(err) => {
-		console.log(err.stack);
-	}
-);
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/shows", passport.authenticate("jwt", {session: false}), require("./routes/shows"));
 app.use("/api/users", passport.authenticate("jwt", {session: false}), require("./routes/users"));
