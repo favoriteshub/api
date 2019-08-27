bef3b3930fcc3ef724e58ebc7c2eabd49c594253
Cristi Ciobanu <c-ciobanu@protonmail.com>
2019-08-27 21:58:47 +0100

    add documentation for users routes

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,39 @@
 const router = require("express").Router();
 const controller = require("../controllers/users");
 
+/**
+ * @swagger
+ *
+ * /api/users:
+ *   post:
+ *     tags:
+ *       - users
+ *     summary: Sign up
+ *     operationId: newUser
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *             required:
+ *               - username
+ *               - password
+ *           example:
+ *             username: test
+ *             password: testtest
+ *             email: test@test.test
+ *     responses:
+ *       200:
+ *         $ref: '#/components/responses/AuthResponse'
+ */
 router.route("/").post(controller.newUser);
 
 module.exports = router;
